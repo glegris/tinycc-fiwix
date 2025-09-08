c33ec24edae4492ee3705fe5ddccf2c98c23c967
Guillaume Legris <guillaume.legris@gmail.com>
2025-09-09 00:02:17 +0200

    Add configure script and fix some code to be able to compile on FiwixOS

---
+++ configure.sh
@@ -0,0 +1,17 @@
+./configure --prefix=/usr --enable-static
+
+printf '%s\n' \
+'#undef CONFIG_TCC_SEMLOCK' \
+'#define CONFIG_TCC_SEMLOCK 0' \
+'#undef CONFIG_TCC_BACKTRACE' \
+'#define CONFIG_TCC_BACKTRACE 0' \
+'#undef CONFIG_TCC_STATIC' \
+'#define CONFIG_TCC_STATIC 1' \
+'#undef CONFIG_TCC_BCHECK' \
+'#define CONFIG_TCC_BCHECK 0' \
+'#undef __ASMNAME' \
+'#define __ASMNAME(x) x' \
+'#undef CONFIG_TCC_SYSINCLUDEPATHS' \
+'#define CONFIG_TCC_SYSINCLUDEPATHS "{B}/include:/usr/i386-pc-fiwix/include:/usr/include/fiwix:/usr/include"' \
+>> config.h
+
